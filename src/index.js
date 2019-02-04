e00d9c080abcb5da9d6237cd3049879dd0d6e48b
Jesse <jesse.david@live.ca>
2019-02-04 00:29:27 -0500

    added approval card

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard />
             <CommentDetail author="Vinutha" message="cool post" />
             <CommentDetail author="Soriya" message="interesting read" />
             <CommentDetail author="Rashmee" message="I want to learn more" />
